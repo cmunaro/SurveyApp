ce8bf0b4518e7fb6d8cc3f1c7f0c0a98cb08dec3
cmunaro <munaro07@gmail.com>
2025-03-10 09:08:42 +0100

    fix: re enable Dispatcher.IO in SurveyRepositoryImpl

--- app/src/main/java/com/example/survey/screens/survey/data/SurveyRepositoryImpl.kt
+++ app/src/main/java/com/example/survey/screens/survey/data/SurveyRepositoryImpl.kt
@@ -5,21 +5,26 @@ import com.example.survey.screens.survey.data.model.QuestionData
 import com.example.survey.screens.survey.data.model.toQuestionsDomain
 import com.example.survey.screens.survey.domain.SurveyRepository
 import com.example.survey.screens.survey.domain.model.QuestionDomain
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 class SurveyRepositoryImpl(
-    private val surveyAPI: SurveyAPI
+    private val surveyAPI: SurveyAPI,
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : SurveyRepository {
     override fun fetchQuestions(): Flow<List<QuestionDomain>> = flow {
         emit(surveyAPI.fetchQuestions())
     }.map(List<QuestionData>::toQuestionsDomain)
+        .flowOn(coroutineDispatcher)
 
     override fun submitAnswer(questionId: Int, answer: String): Flow<Unit> = flow {
         val response = surveyAPI.submitAnswer(
             answer = AnswerData(id = questionId, answer = answer)
         )
         emit(response)
-    }
+    }.flowOn(coroutineDispatcher)
 }
